3233e8a97cb59616c4e622df79f4c0fd4257b75f
saha3336 <naves.maslennikov@ya.ru>
2024-02-06 17:57:25 +0300

    Проверка Дз

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,40 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+        System.out.println("Задача 1");
+        int[] nomber = new int[]{1, 2, 3};
+        System.out.println(Arrays.toString(nomber));
+        double[] nomber1 = new double[]{1.57, 7.654, 9.986};
+        System.out.println(Arrays.toString(nomber1));
+        System.out.println("Задача 1 конец");
+        System.out.println("Задача 2");
+        int[] nomber3 = new int[]{1, 2, 3};
+        System.out.println(Arrays.toString(nomber));
+        double[] nomber2 = new double[]{1.57, 7.654, 9.986};
+        System.out.println(Arrays.toString(nomber1));
+        System.out.println("Задача 2 конец");
+        System.out.println("Задача 3");
+        int[] nomber4 = new int[]{1, 2, 3};
+        for (int i = nomber4.length - 1; i >= 0; i--) {
+            System.out.print(nomber4[i]);
+            if (i != 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        System.out.println("Задача 4");
+        int[] nomber5 = new int[]{1, 2, 3};
+        for (int a = 0; a < nomber5.length; a++) {
+            if (nomber5[a] % 2 != 0) {
+                nomber5[a] = nomber5[a] + 1;
+            }
+        }
+                System.out.println(Arrays.toString(nomber5));
+
+            }
+        }
+
+
+
+
