ec4aa0611129268dc62b5323f330524fce6c640d
saha3336 <naves.maslennikov@ya.ru>
2024-02-11 19:48:42 +0300

    Проверка Дз

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import javax.sound.midi.Soundbank;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fulName = firstName + " " + middleName + " " + lastName;
+        System.out.println( fulName  + " сотрудника");
+        System.out.println(fulName.toUpperCase() + " сотрудника ");
+        String fuLname = "Иванов Семён Семёнович";
+        String  reFulname = fuLname.replace("ё" , "е");
+        System.out.println(reFulname);
+
     }
 }
