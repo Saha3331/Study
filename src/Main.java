0c03f61770d82d211b74502d67b3e80e0470b737
saha3336 <naves.maslennikov@ya.ru>
2024-02-12 22:47:47 +0300

    Проверка Дзm

--- src/Main.java
+++ src/Main.java
@@ -1,117 +1,134 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("задача 1");
-        int depositAmount = 0;
-        int total = 0;
-        while (total <= 2459000) {
-            total = total + 15000;
-            depositAmount++;
-            System.out.println("Месяц " + depositAmount + ", сумма накоплений  " + total + " рблей");
-        }
-        System.out.println("Задача 2");
-        for (int i = 0; i <= 10; i++)
-            System.out.print(i + " ");
-        System.out.println();
-        int a = 10;
-        while (a >= 0) {
-            System.out.print(a + " ");
-            a--;
-        }
-        System.out.println();
-        System.out.println("Звдача 3");
-        int population = 12_000_000;
-        int fertilityPerThousandPeople = 17 * (population / 1000);
-        int mortalityRatePerThousandPeople = 8 * (population / 1000);
-        int populationChange = fertilityPerThousandPeople - mortalityRatePerThousandPeople;
+        {
+            System.out.println(" ");
+            System.out.println("Задача 1");
 
-        for (int time = 0; time < 10; ) {
-            time++;
-            population = population + populationChange;
-            System.out.println("Через " + time + " год, численность населения составляет " + population);
-        }
-    }
-
-    {
-        System.out.println(" ");
-        System.out.println("Задача №4");
-        int monthlyAmount = 15000;
-        int total = 0;
-
-        for (int i = 1; total <= 12_000_000; i++) {
-            total = total + total * 7 / 1000;
-            total = total + monthlyAmount;
-
-            System.out.println("За " + i + " месяцев сумма вклада составит " + total + " рублей");
-        }
-    }
-
-    {
-        System.out.println(" ");
-        System.out.println("Задача №5");
-        int monthlyAmount = 15000;
-        int total = 0;
-        int i = 0;
-
-        while (total <= 12_000_000) {
-            total = total + total * 7 / 1000;
-            total = total + monthlyAmount;
-            i++;
-            if (i % 6 != 0) {
-                System.out.print(" ");
-                continue;
+            int amountDeposit = 15_000;
+            int total = 0;
+            int i = 0;
+            while (total <= 2_469_000) {
+                total = total + total / 100;
+                total = total + amountDeposit;
+                i++;
+                System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей");
             }
-            System.out.println("За " + i + " месяцев сумма вклада составит " + total + " рублей");
         }
-    }
+        {
+            System.out.println(" ");
+            System.out.println("Задача 2");
 
-    {
-        System.out.println(" ");
-        System.out.println("Задача №6");
-        int year = 9;
-        int monthsInYear = 12;
-        int depositPeriodIn = year * monthsInYear;
-        System.out.print("период до  закрытия вклада " + depositPeriodIn + " месяц.");
-        System.out.println(" ");
-        int monthlyAmount = 15000;
-        int total = 0;
-        int i = 0;
-
-        while (i <= depositPeriodIn) {
-            total = total + total * 7 / 1000;
-            total = total + monthlyAmount;
-            i++;
-            if (i % 6 != 0) {
-                System.out.print(" ");
-                continue;
+            int i = 0;
+            while (i < 10) {
+                i++;
+                System.out.print(i + " ");
             }
-            System.out.println("За " + i + " месяцев сумма вклада составит " + total + " рублей");
+            System.out.println();
+            for (; i > 0; i--) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+            System.out.println();
+            for (; i > 0; i--) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
         }
-    }
+        {
+            System.out.println(" ");
+            System.out.println("Задача 3");
 
-    {
-        System.out.println(" ");
-        System.out.println("Задача №7");
+            int population = 12_000_000;
+            int fertilityPerThousandPeople = 17 * (population / 1000);
+            int mortalityRatePerThousandPeople = 8 * (population / 1000);
+            int populationChange = fertilityPerThousandPeople - mortalityRatePerThousandPeople;
 
-        int friday = 2;
-
-        for (; friday < 31; friday = friday + 7) {
-            System.out.println("Сегодня пятница " + friday + " число. Необходимо делать отчет.");
+            for (int time = 0; time < 10; ) {
+                time++;
+                population = population + populationChange;
+                System.out.println("Через " + time + " год, численность населения составляет " + population);
+            }
         }
+        {
+            System.out.println(" ");
+            System.out.println("Задача 4");
 
-    }
+            int monthlyAmount = 15000;
+            int total = 0;
 
-    {
-        System.out.println(" ");
-        System.out.println("Задача №8");
+            for (int i = 1; total <= 12_000_000; i++) {
+                total = total + total * 7 / 1000;
+                total = total + monthlyAmount;
 
-        int year = 2024;
-        int firstTimePeriod = year - 200;
-        int secondTimePeriod = year + 100;
+                System.out.println("За " + i + " месяцев сумма вклада составит " + total + " рублей");
+            }
+        }
+        {
+            System.out.println(" ");
+            System.out.println("Задача 5");
 
-        int i = firstTimePeriod;
-        for (; i >= firstTimePeriod & i < secondTimePeriod; i = i + 79) {
+            int month = 15000;
+            int total = 0;
+            int i = 0;
 
-            System.out.println(i);
+            while (total <= 12_000_000) {
+                total = total + total * 7 / 1000;
+                total = total + month;
+                i++;
+                if (i % 6 != 0) {
+                    System.out.print(" ");
+                    continue;
+                }
+                System.out.println("За " + i + " месяцев сумма вклада составит " + total + " рублей");
+            }
+        }
+        {
+            System.out.println(" ");
+            System.out.println("Задача 6");
+
+            int year = 9;
+            int monthsInYear = 12;
+            int depositPeriodIn = year * monthsInYear;
+            System.out.print("Период до закрытия вклада " + depositPeriodIn + " месяцев.");
+            System.out.println(" ");
+            int month = 15000;
+            int total = 0;
+            int i = 0;
+
+            while (i <= depositPeriodIn) {
+                total = total + total * 7 / 1000;
+                total = total + monthlyAmount;
+                i++;
+                if (i % 6 != 0) {
+                    System.out.print(" ");
+                    continue;
+                }
+                System.out.println("За " + i + " месяцев сумма вклада составит " + total + " рублей");
+            }
+        }
+        {
+            System.out.println(" ");
+            System.out.println("Задача 7");
+
+            int friday = 2;
+
+            for (; friday <= 31; friday = friday + 7) {
+                System.out.println("Сегодня пятница " + friday + " число. Необходимо делать отчет.");
+            }
+
+        }
+        {
+            System.out.println(" ");
+            System.out.println("Задача 8");
+
+            int year = 2024;
+            int firstTimePeriod = year - 200;
+            int secondTimePeriod = year + 100;
+
+            for (int i = firstTimePeriod; i >= firstTimePeriod & i <= secondTimePeriod; i = i + 79) {
+                System.out.println(i);
+            }
+
         }
     }
 }
