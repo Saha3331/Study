2594c3082d128f65a9816454338e4af1597a9abf
saha3336 <naves.maslennikov@ya.ru>
2024-02-17 22:53:47 +0300

    Проверка Дзm

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("8");
+        int year = 2024;
+        int firstTimePeriod = year - 200;
+        int secondTimePeriod = year + 100;
+        int nollePeriod = 0;
+        int rate = 79;
+        while (nollePeriod < secondTimePeriod) {
+            if (nollePeriod > firstTimePeriod) {
+                System.out.println(nollePeriod);
+            }
+            nollePeriod += rate;
+        }
     }
 }
