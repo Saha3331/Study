dea26d6a3bb98c4c039774c1e39674585368bc53
saha3336 <naves.maslennikov@ya.ru>
2024-02-22 18:21:36 +0300

    Проверка Дзm

---
+++ src/Author.java
@@ -0,0 +1,19 @@
+public class Author {
+    public String fistName;
+    public String lastName;
+    public Author() {
+    }
+
+    public Author(String fistName, String lastName) {
+        this.fistName = fistName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "fistName='" + fistName + '\'' +
+                ", lastName='" + lastName + '\''
+                ;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import static jdk.javadoc.internal.doclets.formats.html.markup.HtmlStyle.title;
+
 public class Main {
     public static void main(String[] args) {
+        Author author = new Author();
+        author.lastName = "Иванов";
+        author.fistName = "Иван";
+        System.out.println(author);
+        Book book = new Book("Книга",2022,author);
+        System.out.println(book);
+        System.out.println(book.getYear());
+        book.setYear(2023);
+        System.out.println(book.getYear());
 
-        }
+
+
+
+    }
     }
