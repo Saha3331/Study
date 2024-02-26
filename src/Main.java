d726b93d38538d3f45d7f312738d1532421b5676
saha3336 <naves.maslennikov@ya.ru>
2024-02-26 16:17:01 +0300

    Проверка Gth

--- src/Main.java
+++ src/Main.java
@@ -2,52 +2,66 @@ import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-        int deliveruDisnance = (int) 101f;
-        boolean deliveruDisnanceMax = deliveruDisnance <= 100;
-        PrintDeliveruDisnance(deliveruDisnanceMax, deliveruDisnance);
 
+        // task1();
+        //task2();
 
-        System.out.println();
-        int clientOS = 0;
-        int clientDeviceYear = 2015;
-        recommedAppVersion(clientOS, clientDeviceYear);
-
-        System.out.println("1");
-
-
-        printIsLeapYearResult(2020);
-        printIsLeapYearResult(2021);
-        printIsLeapYearResult(2022);
-        printIsLeapYearResult(2023);
-        printIsLeapYearResult(2024);
-    }
-
-    private static void PrintDeliveruDisnance(boolean deliveruDisnanceMax, int deliveruDisnance) {
-        if (deliveruDisnanceMax) {
-            if (deliveruDisnance <= 20) {
-                System.out.println("Потребуются 1 сутки для доставки вашей банковской карты.");
-            } else if (deliveruDisnance > 20 && deliveruDisnance <= 60) {
-                System.out.println("Потребуются 2 суток для доставки вашей банковской карты.");
-            } else if (deliveruDisnance > 60 && deliveruDisnance <= 100) {
-                System.out.println("Потребуются 3 суток для доставки вашей банковской карты.");
-            }
+        int deliveryDistance = 15;
+        int deliveryTime = printDeliveryDistance(deliveryDistance);
+        if (deliveryTime == -1) {
+            System.out.println("Нет доставки");
         } else {
-            System.out.println("Доставки нет.");
+            System.out.println("потребуеться дней" + deliveryTime);
         }
     }
 
-    private static void recommedAppVersion(int clientOS, int clientDeviceYear) {
-        int currentYear = LocalDate.now().getYear();
-        if (clientOS == 1 && clientDeviceYear <= currentYear) {
-            System.out.println("Установите облегченную версию приложения для Android по ссылке");
-        } else if (clientOS == 0 && clientDeviceYear < 2015) {
-            System.out.println("Установите облегченную версию приложения для iOS по ссылке:");
-        } else if (clientOS == 1 && clientDeviceYear >= 2015) {
-            System.out.println("Установите версию приложения для Android по ссылке");
-        } else if (clientOS == 0 && clientDeviceYear >= 2015) {
-            System.out.println("Установите версию приложения для iOS по ссылке");
-        } else
-            System.out.println("Error");
+    private static int printDeliveryDistance(int deliveryDistance) {
+        int deliveryTime = 1;
+        if (deliveryDistance > 100) {
+            deliveryTime = -1;
+        }else {
+            if (deliveryDistance > 20 && deliveryDistance <=60) {
+                deliveryTime++;
+            }else if (deliveryDistance > 60){
+                deliveryTime +=2;
+            }
+        }
+        return deliveryTime;
+    }
+
+    private static void task2() {
+        int clientOS = 0;
+        int clientDeviceYear = 2015;
+        recommendAppVersion(clientOS, clientDeviceYear);
+    }
+
+    private static void recommendAppVersion(int clientOS, int clientDeviceYear) {
+        if (clientOS != 1 && clientOS != 1 ) {
+            System.out.println(" неизвестная сист");
+        } else {
+            String opertionSystem;
+            if (clientOS ==0) {
+                opertionSystem = "IOS";
+            }else {
+                opertionSystem = "Android";
+            }
+            String message;
+            if (isDevaisOld(clientDeviceYear)){
+                message = "Установить облегчённую версию приложения для "+ opertionSystem + "по ссылке";
+            }else {
+                message = "Установить версию прилоения для "+ opertionSystem +"по сыылке";
+            }
+            System.out.println(message);
+        }
+    }
+
+    private static boolean isDevaisOld(int clientDeviceYear) {
+        int currentYear =  LocalDate.now().getYear();
+        return clientDeviceYear <= currentYear;
+    }
+
+    private static void task1() {
+        printIsLeapYearResult(2021);
     }
 
     private static void printIsLeapYearResult(int year) {
@@ -57,6 +71,8 @@ public class Main {
             System.out.println(year + "Год не являеться высокосным");
         }
     }
+
+
 
     private static boolean isLeap(int year) {
         return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
